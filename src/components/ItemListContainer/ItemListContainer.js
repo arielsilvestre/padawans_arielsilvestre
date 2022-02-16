c1a67c94445de95a795dd9461c99ffaf589df418
arielsilvestre <silvestreariel@gmail.com>
2022-02-15 21:08:27 -0300

    Desafio clase 6 | Promesas

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,19 @@
 import ItemCount from "../ItemCount/ItemCount"
+import ItemList from "../ItemList/ItemList";
+import { getProducts } from "../../asyncmock";
+import { useEffect, useState } from "react";
 
 const ItemListContainer = ({greeting}) =>{ //le paso props al boton
+    const[products,setProducts] = useState([])
+
+    useEffect(()=>{
+        getProducts().then(products=>{
+            console.log(products)
+            setProducts(products)
+        })
+    })
+
+
     const handleOnAdd = (quantity) => {
         console.log(`se agregaron ${quantity} productos`);
     }
@@ -9,6 +22,19 @@ const ItemListContainer = ({greeting}) =>{ //le paso props al boton
         <>
             <h1>{greeting}</h1>
             <ItemCount stock={10} initial={2} onAdd={handleOnAdd}></ItemCount>
+                {/* {products.map(product=>{
+                    return <li key={product.id}>{product.name}</li>
+                })} */}
+                {products.map(product=>{
+                     return <div style={{fontSize:50}}>
+                                <h5>{product.name}</h5>
+                                <img src={product.img}></img>
+                                <h6>{product.price}</h6>
+                                <p style={{color:'grey', fontSize:10}}>{product.category}</p>
+                                <button style={{background:'#44FFFF'}}>Comprar</button>
+                            </div>
+                })}
+           
         </>
     )
 }
